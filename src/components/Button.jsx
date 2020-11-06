afe22c4307510dd80b9f5e03e18bb68383eaea1c
30003oleg <60287657+30003oleg@users.noreply.github.com>
2020-11-06 12:36:25 +0300

    1

---
+++ src/components/Button.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Button extends React.Component {
+
+    componentDidMount() {
+        console.log ('mounted');
+    }
+    componentDidUpdate () {
+        
+    }
+
+
+    render () {
+        return (
+            <button>fdfd</button>
+        )
+    }
+}
+
+export default Button;
