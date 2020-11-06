2d5e262085e353066e052d2aead68f81c2719b64
30003oleg <60287657+30003oleg@users.noreply.github.com>
2020-11-06 18:50:00 +0300

    1

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import classNames from 'classnames';
 
 
-class Button extends React.Component {
+/*class Button extends React.Component {
 
     componentDidMount() {
         console.log ('mounted');
@@ -18,6 +18,12 @@ class Button extends React.Component {
             <button className={classNames('button', {'button--outline':this.props.outline})}>{this.props.value}</button>
         )
     }
+}*/
+
+function Button(props) {
+    return (
+        <button className={classNames('button', {'button--outline':props.outline})}>{props.value}</button>
+    );   
 }
 
 export default Button;
